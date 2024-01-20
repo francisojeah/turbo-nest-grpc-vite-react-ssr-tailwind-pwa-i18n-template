310c4f19ee58339ca124fe33775c40d8df1e8aa1
francisojeah <ojeahfrancis@gmail.com>
2024-01-20 08:47:40 +0100

    Implemented Swagger integration for enhanced API documentation and exploration

--- hms-apps/api-gateway/src/main.ts
+++ hms-apps/api-gateway/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,24 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
 
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(process.env.APP_NAME)
+    .setDescription(process.env.APP_DESCRIPTION)
+    .setVersion(process.env.API_VERSION)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig, {
+    ignoreGlobalPrefix: false,
+  });
+
+  try {
+    SwaggerModule.setup('api', app, document, {
+      swaggerOptions: {
+        baseUrl: '/v1',
+      },
+    });
+  } catch (error) {
+    this.logger.error(error);
+  }
   
   await app.listen(3002);
 }
