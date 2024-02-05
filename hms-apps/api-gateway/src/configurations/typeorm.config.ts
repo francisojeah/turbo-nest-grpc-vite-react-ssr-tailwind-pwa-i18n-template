b0e15f7263cdce5e0077b567e3d14c0bc0591882
francisojeah <ojeahfrancis@gmail.com>
2024-02-05 21:59:13 +0100

    changed database to mysql

--- hms-apps/api-gateway/src/configurations/typeorm.config.ts
+++ hms-apps/api-gateway/src/configurations/typeorm.config.ts
@@ -4,29 +4,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 require('dotenv').config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  username: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  url: process.env.POSTGRES_URL,
-  entities: [
-    // Auth,
-    // Course,
-    // LecturerCourses,
-    // Lecturer,
-    // Program,
-    // School,
-    // StudentCourse,
-    // Student,
-    // User,
-  ],
-  migrations: [__dirname + '/../database/migrations/*{.ts,.js}'], // Migration files directory
-  extra: {
-    charset: 'utf8mb4_unicode_ci',
-  },
-  synchronize: false, // Auto-create database tables based on entities (not recommended for production)
-  autoLoadEntities: true, // Automatically load entity files
+  type: 'mysql',
+  port: 3306,
+  host: 'localhost',
+  username: 'root',
+  password: 'root',
+  synchronize: true,
+  autoLoadEntities: true,
   logging: false, // Disable logging SQL queries
   ssl: {
     rejectUnauthorized: false, // Reject unauthorized SSL connections
