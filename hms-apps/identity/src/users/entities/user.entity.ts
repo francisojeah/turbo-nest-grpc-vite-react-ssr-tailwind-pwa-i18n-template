ff8dfa32a7666efb04c9a2470902464a60b70b3e
francisojeah <ojeahfrancis@gmail.com>
2024-02-05 15:16:43 +0100

    updated the id field to compulsory

--- hms-apps/identity/src/users/entities/user.entity.ts
+++ hms-apps/identity/src/users/entities/user.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity()
 export class User {
   @PrimaryGeneratedColumn("uuid")
-  id?: string;
+  id: string;
 
   @Column({ unique: true })
   primaryEmailAddress: string;
