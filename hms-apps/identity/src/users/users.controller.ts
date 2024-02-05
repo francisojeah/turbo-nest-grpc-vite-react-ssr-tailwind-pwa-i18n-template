82e8bc1d3b122a98d6cddd8fd2539fff75b35b21
francisojeah <ojeahfrancis@gmail.com>
2024-02-05 15:14:10 +0100

    updated implementation for controller functions in user controller for identity microservice

--- hms-apps/identity/src/users/users.controller.ts
+++ hms-apps/identity/src/users/users.controller.ts
@@ -3,7 +3,7 @@ import { Controller } from '@nestjs/common';
 import { UsersService } from './users.service';
 //import { CreateUserDto } from './dto/create-user.dto';
 //import { UpdateUserDto } from './dto/update-user.dto';
-import { UsersServiceController, CreateUserDto, UpdateUserDto, UsersServiceControllerMethods, FindOneUserDto, PaginationDto, FindOneUserByPrimaryEmailAddressDto } from '@common/hms-lib';
+import { UsersServiceController, CreateUserDto, UpdateUserDto, UsersServiceControllerMethods, FindOneUserDto, PaginationDto, FindOneUserByPrimaryEmailAddressDto, User } from '@common/hms-lib';
 import { Observable } from 'rxjs';
 
 @Controller()
@@ -11,32 +11,31 @@ import { Observable } from 'rxjs';
 export class UsersController implements UsersServiceController {
   constructor(private readonly usersService: UsersService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async createUser(request: CreateUserDto) {
+    return await this.usersService.create(request);
   }
 
-  findAll() {
-    return this.usersService.findAll();
+  async findAllUsers() {
+    return await this.usersService.findAll();
   }
 
-  findOne(findOneUserDto: FindOneUserDto) {
-    return this.usersService.findOne(findOneUserDto.id);
+  async findOneUser(findOneUserDto: FindOneUserDto) {
+    return await this.usersService.findOne(findOneUserDto.id);
   }
 
-  update(updateUserDto: UpdateUserDto) {
-    return this.usersService.update(updateUserDto.id, updateUserDto);
+  async updateUser(updateUserDto: UpdateUserDto) {
+    return await this.usersService.update(updateUserDto.id, updateUserDto);
   }
 
-  remove(findOneUserDto: FindOneUserDto) {
-    return this.usersService.remove(findOneUserDto.id);
+  async removeUser(findOneUserDto: FindOneUserDto) {
+    return await this.usersService.remove(findOneUserDto.id);
   }
 
   queryUsers(paginationDtoStream: Observable<PaginationDto>) {
     return this.usersService.queryUsers(paginationDtoStream);
-      
   }
 
   async findOneUserByPrimaryEmailAddress(findOneUserByPrimaryEmailAddressDto: FindOneUserByPrimaryEmailAddressDto){
-    return this.usersService.findOneUserByPrimaryEmailAddress(findOneUserByPrimaryEmailAddressDto.primaryEmailAddress);
+    return await this.usersService.findOneUserByPrimaryEmailAddress(findOneUserByPrimaryEmailAddressDto.primaryEmailAddress);
   }
 }
