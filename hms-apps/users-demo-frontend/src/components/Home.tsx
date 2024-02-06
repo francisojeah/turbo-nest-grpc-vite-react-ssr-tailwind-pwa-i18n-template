44e61993d07038e939857a6aa94bbee9b7719aa6
francisojeah <ojeahfrancis@gmail.com>
2024-02-06 17:40:22 +0100

    Implemented i18n for home page

--- hms-apps/users-demo-frontend/src/components/Home.tsx
+++ hms-apps/users-demo-frontend/src/components/Home.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import { H1 } from "../../@/components/ui/typography";
 
 type Props = {
@@ -5,12 +6,33 @@ type Props = {
 }
 
 const Home:React.FC<Props> = () => {
+
+  //Below uses internationalization for menu, unlike above
+  const { t, i18n } = useTranslation();
+  //Or You can narrow down useTranslation() to one or more namespaces as shown below
+  //const { t, i18n } = useTranslation('adm'); //narrow down to a namespace or array of namespaces if necessary
+  //const { t, i18n } = useTranslation(['public','adm']);
+
+  //prepare language util
+
+  //@ts-ignore //language could be passed down to everywhere. Not in use now. hence //@ts-ignore
+  const [language, setLanguage] = useState(i18n.language);
+
+  //Below is an example of a callback function called from a select event to change language. Not in use here, hence the //@ts-ignore
+  //@ts-ignore
+  const changeLanguage = async (e: { target: { value: string | undefined; }; }) => {
+      //Notice that in the default lng in i18n.js is en. Of course, you should make this more state managed with a language selection UI
+      await i18n.changeLanguage(e.target.value); //set the language
+      setLanguage(i18n.language);//update state to provoke rerender
+  }
+
+  
     return (
         <>
         <section className="bg-white dark:bg-gray-900">
           <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
             <H1>
-              This is the Home Page
+              {t('home page')}
             </H1>
           </div>
         </section>
